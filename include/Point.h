0e273e5ddde8a188821edb92f0945f7e3deb285e
Unclesdad <basaltjazz3@gmail.com>
2026-02-21 23:30:54 -0500

    docs and implementations of Point

--- include/Point.h
+++ include/Point.h
@@ -1,10 +1,29 @@
 #pragma once
 
+#include "Vector.h"
+
 class Point {
 public:
-    double x;
-    double y;
+    /// @brief x-coordinate of the point.
+    const double x;
+    /// @brief y-coordinate of the point.
+    const double y;
 
+    /// @brief Constructs a point at (0,0).
     Point();
+
+    /// @brief Constructs a point at (x,y).
+    /// @param x The x-coordinate of the point.
+    /// @param y The y-coordinate of the point.
     Point(double x, double y);
+
+    /// @brief Constructs a point translated from the origin by a vector. 
+    /// The set of points in two-space is isomorphic to R2 after all.
+    /// @param vector The vector of translation from the origin.
+    Point(const Vector& vector);
+
+    /// @brief Finds the displacement between two points as a vector.
+    /// @param rhs The starting point.
+    /// @return The displacement between two points.
+    Vector operator-(const Point& rhs) const;
 };
--- src/Point.cpp
+++ src/Point.cpp
@@ -3,3 +3,9 @@
 Point::Point() : x(0), y(0) {}
 
 Point::Point(double x, double y) : x(x), y(y) {}
+
+Point::Point(const Vector& vector) : x(vector.x), y(vector.y) {}
+
+Vector Point::operator-(const Point& rhs) const {
+    return Vector(x - rhs.x, y - rhs.y);
+}
