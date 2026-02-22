eeac3c0f126275d2bdec3313e9ceb4f7b3cdae22
Unclesdad <basaltjazz3@gmail.com>
2026-02-21 23:42:14 -0500

    filled out Vector.h and added documentation

--- include/Vector.h
+++ include/Vector.h
@@ -25,4 +25,38 @@ public:
 
     /// @return The magnitude (or length) of the vector.
     double magnitude() const;
+
+    /// @return The angle of the vector with the horizontal.
+    Angle angle() const;
+
+    /// @brief Adds two vectors.
+    /// @param rhs The vector to add to the vector on the left side of the addition sign.
+    /// @return The sum of two vectors.
+    Vector operator+(const Vector& rhs) const;
+
+    /// @brief Subtracts two vectors.
+    /// @param rhs The vector to subtract from the vector on the left of the addition sign.
+    /// @return The difference of two vectors.
+    Vector operator-(const Vector& rhs) const;
+
+    /// @brief Multiplies a vector by a scalar.
+    /// @param rhs The scalar multiple.
+    /// @return The scalar multiplication of this vector.
+    Vector operator*(double rhs) const;
+
+    /// @brief Multiplies a vector by a scalar.
+    /// @param lhs The scalar multiple.
+    /// @param rhs The vector to scalar multiply.
+    /// @return The scalar multiplication of a vector.
+    friend Vector operator*(double lhs, const Vector& rhs);
+
+    /// @brief Calculates the dot product of this vector with another.
+    /// @param rhs The vector to dot with.
+    /// @return The dot product of two vectors.
+    double dot(const Vector& rhs) const;
+
+    /// @brief Calculates the magnitude of the cross product of this vector with another.
+    /// @param rhs the vector to cross with.
+    /// @return The magnitude of the cross product of two vectors.
+    double cross(const Vector& rhs) const;
 };
