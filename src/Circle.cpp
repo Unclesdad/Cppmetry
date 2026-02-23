febcc39fbce8182a37063ef51d47633ffdb073d2
Unclesdad <basaltjazz3@gmail.com>
2026-02-23 16:58:59 -0500

    fixed Circle::intersectsPerimeter so that secant lines are also detected

--- src/Circle.cpp
+++ src/Circle.cpp
@@ -3,5 +3,10 @@
 Circle::Circle(const Point& center, double radius) : center(center), radius(radius) {}
 
 bool Circle::intersectsPerimeter(const Line& line) const {
-    return ((line.p1 - center).magnitude() > radius) != ((line.p2 - center).magnitude() > radius);
+    // check if one endpoint is on inside and one is on outside
+    if (((line.p1 - center).magnitude() > radius) != ((line.p2 - center).magnitude() > radius)) return true;
+    
+    // check if line is a secant and intersects circle edge. nearest dist to line should be less than or eq to radius
+    Vector endpointDisplacement = line.p2 - center;
+    return (endpointDisplacement - endpointDisplacement.projectOnto(line.toVector())).magnitude() <= radius;
 }
